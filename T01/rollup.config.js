f83b0839a88014a3586bba33630cee1637aa1bef
PML30\cgsg257 <shiryaeva.daria.al@gmail.com>
2026-06-04 19:38:00 +0300

    T01: making project of it.. in process..

---
+++ T01/rollup.config.js
@@ -0,0 +1,30 @@
+/*
+module.exports = {
+    input: "src/main.js",
+    output: {
+        file: "dist/main.js",
+        format: "iife"
+    }
+};
+*/
+const resolve=require("@rollup/plugin-node-resolve")
+const commonjs=require("@rollup/plugin-commonjs")
+const terser=require("@rollup/plugin-terser")
+
+module.exports = {
+    input: "src/main.js",
+    output: {
+        dir: "dist",
+        format: "iife",
+        sourcemap: "inline"
+    },
+    plugins:[
+        resolve({
+            jsnext: true,
+            main: true,
+            browser: true
+        }),
+        commonjs(),
+        // terser()
+    ]
+};
---
+++ T01/src/main.js
@@ -0,0 +1,8 @@
+import { add } from './math.js'
+import { Pane } from 'tweakpane'
+
+window.addEventListener("load", () => {
+    const pane = new Pane();
+
+    console.log("ab");
+})
